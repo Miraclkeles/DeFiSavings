a2812602a1e7fb5b999b4151e4f7f89497519d01
Miraç KELEŞ <64755870+Miraclkeles@users.noreply.github.com>
2024-06-06 10:56:44 +0200

    update variable names

--- index.tsx
+++ index.tsx
@@ -1,11 +1,16 @@
+// Importing necessary libraries and components
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './App'; // This is the main component of our application
+import './index.css'; // Global styles for the entire application
 
+// Identifying the root element from the HTML document where our React app will be mounted.
+const appMountPoint = document.getElementById('root');
+
+// Rendering our App component within the React.StrictMode wrapper for additional checks and warnings in development mode.
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  appMountPoint // Mounting the App component to the DOM.
 );
